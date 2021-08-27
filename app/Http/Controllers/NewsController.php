62bddfae9e593de446a8841100a864e951094508
Arifin Tripto <arifin.design94@gmail.com>
2021-08-27 17:17:37 +0600

    cleaner approach to call API

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,26 +9,27 @@ use jcobhams\NewsApi\NewsApi;
 
 class NewsController extends Controller
 {
+    public function callNewsAPI($category) {
+        $api_key = 'b6d11937c33c40178630366ba59687e3';
+        $newsapi = new NewsApi($api_key);
+
+        $top_headlines = $newsapi->getTopHeadlines('', NULL, 'us', $category, 20, 1);
+
+        return $top_headlines;
+    }
+
     public function index($category) {
 
 //        $response = Http::get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b6d11937c33c40178630366ba59687e3');
 //        dd($response->json('articles'));
         //This code would also work but I found NewsAPI's own package on their website and used that.
 
-        $api_key = 'b6d11937c33c40178630366ba59687e3';
-        $newsapi = new NewsApi($api_key);
-
-        $top_headlines = $newsapi->getTopHeadlines('', NULL, 'us', $category, 20, 1);
-        $articles = $top_headlines->articles;
+        $articles = $this->callNewsAPI($category)->articles;
         return view('news', compact('articles', 'category'));
     }
 
     public function show($category, $id) {
-        $api_key = 'b6d11937c33c40178630366ba59687e3';
-        $newsapi = new NewsApi($api_key);
-
-        $top_headlines = $newsapi->getTopHeadlines('', NULL, 'us', $category, 20, 1);
-        $article = $top_headlines->articles[$id];
+        $article = $this->callNewsAPI($category)->articles[$id];
 
         return view('news_details', compact('article', 'category'));
     }
